d72023e2053aeabb32266ef90d93e4027b9868a2
Marcio Vinicius de Souza da Rocha <marcio.souza@mps.com.br>
2022-02-15 16:55:10 -0300

    iniciado desenvolvimento de script para inicar novos projetos de maneira mais efetiva

---
+++ create_project.sh
@@ -0,0 +1,47 @@
+#!/bin/bash 
+
+verificar_parametros(){
+    if -d "$f" ; then
+        echo "A pasta especificada ('${f}') não existe."
+        return 1
+    fi
+    if -z "$e" ; then
+        echo "Extensão do arquivo principal não especificada."
+        return 1
+    fi
+    if -z "$n" ; then
+        echo "Nome do projeto não especificado."
+        return 1
+    fi
+    return 0
+}
+
+# this script will create an initial folder and some files for projects
+f="./"
+while getopts "f:n:" flag; do 
+    case "${flag}" in
+        f) f="${OPTARG}" ;; # especifica a pasta em que será criado o repositório
+        n) n="${OPTARG}" ;; # nome do projeto
+        e) e="${OPTARG}" ;; # extensao do arquivo principal
+    esac
+done
+
+if ! verificar_parametros ; then
+    exit 1
+fi
+
+f="${f}/${n}"
+echo "Criando pasta do projeto..."
+mkdir "${f}" && echo "Criada pasta do projeto: '${f}/${n}'"
+
+echo "Searching files to create"
+dependencias -t 'c' -f "/data/initial_files"
+
+echo "Criando arquivos..."
+touch "${f}/${n}.${e}" && echo "Criado arquivo: '${f}/${n}.${e}'"
+while IFS= read -r file ; do
+    touch "${f}/${file}" && echo "Criado arquivo: '${file}'" 
+done < "./data/initial_files"
+
+echo "Iniciando repositório GIT..."
+cd "${f}" && git init
